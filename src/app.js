9bd1d45c0516234f606126c727a309dd29981dcf
Delfin656 <juandelfin656@gmail.com>
2022-08-15 21:09:52 +0000

    Proyecto domain name generator js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("hello world");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var exten = [".com"];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let e = 0; e < exten.length; e++) {
+          console.log(pronoun[i] + adj[a] + noun[n] + exten[e]);
+        }
+      }
+    }
+  }
 };
